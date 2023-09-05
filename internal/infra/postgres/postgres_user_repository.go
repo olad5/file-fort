25e96aae0fd76e54a0e9c930676bf9a766443f8c
Francis Shonubi <shonubioladipo@gmail.com>
2023-09-05 10:01:28 +0100

    refactor: postgres user repository
    - moved to sqlx

--- internal/infra/postgres/postgres_user_repository.go
+++ internal/infra/postgres/postgres_user_repository.go
@@ -2,31 +2,26 @@ package postgres
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
+	"github.com/jmoiron/sqlx"
 	"github.com/olad5/go-cloud-backup-system/internal/domain"
 	"github.com/olad5/go-cloud-backup-system/internal/infra"
 )
 
-type PostgresRepository struct {
-	connection *pgx.Conn
+type PostgresUserRepository struct {
+	connection *sqlx.DB
 }
 
-func NewPostgresRepo(ctx context.Context, DatabaseUrl string) (*PostgresRepository, error) {
-	connection, err := pgx.Connect(ctx, DatabaseUrl)
-	if err != nil {
-		return &PostgresRepository{}, fmt.Errorf("Failed to create PostgresRepository:  %w", err)
+func NewPostgresUserRepo(ctx context.Context, connection *sqlx.DB) (*PostgresUserRepository, error) {
+	if connection == nil {
+		return &PostgresUserRepository{}, fmt.Errorf("Failed to create PostgresFileRepository: connection is nil")
 	}
 
-	return &PostgresRepository{connection: connection}, nil
-}
-
-func (p *PostgresRepository) Migrate(ctx context.Context) error {
-	query := `
+	const userSchema = `
   CREATE TABLE IF NOT EXISTS users(
       id UUID PRIMARY KEY,
       email TEXT NOT NULL UNIQUE,
@@ -38,63 +33,92 @@ func (p *PostgresRepository) Migrate(ctx context.Context) error {
   );
 
 `
-	_, err := p.connection.Exec(ctx, query)
-	if err != nil {
-		return fmt.Errorf("Failed to Migrate Postgres DB:  %w", err)
-	}
-
-	return nil
+	connection.MustExec(userSchema)
+	return &PostgresUserRepository{connection: connection}, nil
 }
 
-func (p *PostgresRepository) CreateUser(ctx context.Context, user domain.User) error {
-	err := p.Migrate(ctx)
-	if err != nil {
-		return err
-	}
-
-	query := `
-    INSERT INTO users(id, first_name, last_name, email, password, role) values($1, $2, $3, $4, $5, $6) RETURNING id
+func (p *PostgresUserRepository) CreateUser(ctx context.Context, user domain.User) error {
+	const query = `
+    INSERT INTO users
+      (id, first_name, last_name, email, password, role) 
+    VALUES 
+      (:id, :first_name, :last_name, :email, :password, :role)
   `
 
-	err = p.connection.QueryRow(ctx, query, user.ID, user.FirstName, user.LastName, user.Email, user.Password, user.Role).Scan(nil)
-	return err
-}
-
-func (p *PostgresRepository) GetUserByEmail(ctx context.Context, userEmail string) (domain.User, error) {
-	err := p.Migrate(ctx)
+	_, err := p.connection.NamedExec(query, toSqlxUser(user))
 	if err != nil {
-		return domain.User{}, err
+		return fmt.Errorf("error creating user in the db: %w", err)
 	}
-	row := p.connection.QueryRow(ctx, "SELECT * FROM users WHERE email = $1", userEmail)
-
-	var id uuid.UUID
-	var first_name string
-	var email string
-	var last_name string
-	var password string
-	var role domain.UserRole
-
-	if err := row.Scan(&id, &email, &first_name, &last_name, &password, &role); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return domain.User{}, errors.New(infra.ErrRecordNotFound)
-		}
-	}
-
-	return domain.User{
-		ID:        id,
-		Email:     email,
-		FirstName: first_name,
-		LastName:  last_name,
-		Password:  password,
-		Role:      role,
-	}, nil
-}
-
-func (p *PostgresRepository) Ping(ctx context.Context) error {
-	err := p.connection.Ping(ctx)
-	if err != nil {
-		return fmt.Errorf("Failed to Ping Postgres DB:  %w", err)
-	}
-
 	return nil
+}
+
+func (p *PostgresUserRepository) GetUserByEmail(ctx context.Context, userEmail string) (domain.User, error) {
+	var user SqlxUser
+
+	err := p.connection.Get(&user, "SELECT * FROM users WHERE email = $1", userEmail)
+	if err != nil {
+		if errors.Is(err, ErrRecordNotFound) {
+			return domain.User{}, infra.ErrUserNotFound
+		}
+		return domain.User{}, fmt.Errorf("error getting user by email: %w", err)
+	}
+	return toUser(user), nil
+}
+
+func (p *PostgresUserRepository) GetUserByUserId(ctx context.Context, userId string) (domain.User, error) {
+	var user SqlxUser
+
+	err := p.connection.Get(&user, "SELECT * FROM users WHERE id = $1", userId)
+	if err != nil {
+		if errors.Is(err, ErrRecordNotFound) {
+			return domain.User{}, infra.ErrUserNotFound
+		}
+		return domain.User{}, fmt.Errorf("error getting user by userId: %w", err)
+	}
+	return toUser(user), nil
+}
+
+func (p *PostgresUserRepository) Ping(ctx context.Context) error {
+	err := p.connection.Ping()
+	if err != nil {
+		return fmt.Errorf("Failed to Ping PostgresUserRepository:  %w", err)
+	}
+	return nil
+}
+
+type SqlxUser struct {
+	ID        uuid.UUID       `db:"id"`
+	Email     string          `db:"email"`
+	FirstName string          `db:"first_name"`
+	LastName  string          `db:"last_name"`
+	Password  string          `db:"password"`
+	Role      domain.UserRole `db:"role"`
+	CreatedAt time.Time       `db:"created_at"`
+	UpdatedAt time.Time       `db:"updated_at"`
+}
+
+func toUser(u SqlxUser) domain.User {
+	return domain.User{
+		ID:        u.ID,
+		Email:     u.Email,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Password:  u.Password,
+		Role:      u.Role,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+	}
+}
+
+func toSqlxUser(u domain.User) SqlxUser {
+	return SqlxUser{
+		ID:        u.ID,
+		Email:     u.Email,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Password:  u.Password,
+		Role:      u.Role,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+	}
 }
