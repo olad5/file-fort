34b0cf2d6536e47568ff5c38831f5b544e2ee97c
Francis Shonubi <shonubioladipo@gmail.com>
2023-09-05 09:55:06 +0100

    feat: add folder repository

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/olad5/go-cloud-backup-system
 go 1.20
 
 require (
+	github.com/aws/aws-sdk-go v1.44.331 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
@@ -12,7 +13,10 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 )
---
+++ internal/infra/postgres/postgres_folder_repository.go
@@ -0,0 +1,98 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+
+	"github.com/olad5/go-cloud-backup-system/internal/domain"
+	"github.com/olad5/go-cloud-backup-system/internal/infra"
+)
+
+type PostgresFolderRepository struct {
+	connection *sqlx.DB
+}
+
+func NewPostgresFolderRepo(ctx context.Context, connection *sqlx.DB) (*PostgresFolderRepository, error) {
+	if connection == nil {
+		return &PostgresFolderRepository{}, fmt.Errorf("Failed to create PostgresFolderRepository: connection is nil")
+	}
+	const folderSchema = `
+  CREATE TABLE IF NOT EXISTS folders(
+      id UUID PRIMARY KEY,
+      folder_name varchar(255) NOT NULL,
+      owner_id UUID NOT NULL REFERENCES users(id), 
+      "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
+
+`
+
+	connection.MustExec(folderSchema)
+	return &PostgresFolderRepository{connection: connection}, nil
+}
+
+func (p *PostgresFolderRepository) CreateFolder(ctx context.Context, folder domain.Folder) error {
+	query := `
+    INSERT INTO folders (id, folder_name, owner_id) VALUES (:id, :folder_name, :owner_id)
+  `
+
+	_, err := p.connection.NamedExec(query, toSqlxFolder(folder))
+	if err != nil {
+		return fmt.Errorf("error creating folder in the db: %w", err)
+	}
+	return nil
+}
+
+func (p *PostgresFolderRepository) GetFolderByFolderId(ctx context.Context, folderId string) (domain.Folder, error) {
+	var folder SqlxFolder
+	err := p.connection.Get(&folder, "SELECT * FROM folders WHERE id=$1", folderId)
+	if err != nil {
+		if err == ErrRecordNotFound {
+			return domain.Folder{}, infra.ErrFolderNotFound
+		}
+		return domain.Folder{}, fmt.Errorf("error getting folder :%w", err)
+	}
+
+	return toDomainFolder(folder), nil
+}
+
+func (p *PostgresFolderRepository) Ping(ctx context.Context) error {
+	err := p.connection.Ping()
+	if err != nil {
+		return fmt.Errorf("Failed to Ping PostgresFolderRepository:  %w", err)
+	}
+
+	return nil
+}
+
+type SqlxFolder struct {
+	ID         uuid.UUID `db:"id"`
+	FolderName string    `db:"folder_name"`
+	OwnerId    uuid.UUID `db:"owner_id"`
+	CreatedAt  time.Time `db:"created_at"`
+	UpdatedAt  time.Time `db:"updated_at"`
+}
+
+func toDomainFolder(f SqlxFolder) domain.Folder {
+	return domain.Folder{
+		ID:         f.ID,
+		FolderName: f.FolderName,
+		OwnerId:    f.OwnerId,
+		CreatedAt:  f.CreatedAt,
+		UpdatedAt:  f.UpdatedAt,
+	}
+}
+
+func toSqlxFolder(f domain.Folder) SqlxFolder {
+	return SqlxFolder{
+		ID:         f.ID,
+		FolderName: f.FolderName,
+		OwnerId:    f.OwnerId,
+		CreatedAt:  f.CreatedAt,
+		UpdatedAt:  f.UpdatedAt,
+	}
+}
